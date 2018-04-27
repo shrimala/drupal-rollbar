e9e50257326d211e076e181be2ddc4b3d27fc5e6
Timothy Groves <groves@greatwhiteark.com>
2018-04-27 17:08:28 +0200

    Initial commit.

---
+++ src/Subscriber/EventSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\drupal_rollbar\Subscriber;
+
+use Rollbar\Payload\Level;
+use Rollbar\Rollbar;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\KernelEvent;
+
+/**
+ * Subscribes to kernel event to init Rollbar.
+ */
+class EventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events['kernel.request'][] = ['onKernelRequest'];
+    return $events;
+  }
+
+  /**
+   * @param Symfony\Component\HttpKernel\Event\KernelEvent $event
+   */
+  public function onKernelRequest(KernelEvent $event) {
+    // Set up rollbar PHP.
+    if ($access_token = getenv('ROLLBAR_ACCESS_TOKEN')) {
+      $environment = getenv('ROLLBAR_ENVIRONMENT') ?: 'production';
+
+      Rollbar::init(
+        [
+          'access_token' => $access_token,
+          'environment' => $environment,
+        ]
+      );
+
+      Rollbar::log(Level::INFO, 'testing info level');
+    }
+  }
+
+}
